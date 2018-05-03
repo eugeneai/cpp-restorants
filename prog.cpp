f9a7530f2c04e94f910d3dfc8eb3117e4a6d8507
Evgeny Cherkashin <eugeneai@irnok.net>
2018-05-03 13:06:19 +0800

    Refactoring for simplification.

--- prog.cpp
+++ prog.cpp
@@ -2,6 +2,8 @@
 #include "stdio.h"
 #include <stdlib.h>
 #include <vector>
+#include <map>
+#include <csignal>
 
 #define NL "\n"
 
@@ -12,11 +14,6 @@ using namespace std;
 
 // Depth-first constraint satisfation.
 
-
-struct Arc {
-  int a,b;   // int m_a, m_b
-  Arc(int a, int b);
-};
 
 enum state_t{
   MIN=0,
@@ -29,12 +26,14 @@ enum state_t{
 
 
 class Rest {
-  vector<Arc *> * v = NULL;
+  multimap<int,int> * m = NULL;
   vector<state_t> * s = NULL;
+  int size = 0;
 
 public:
   bool csp (int index, state_t con);
-  Arc * add(int a, int b);
+  void add(int a, int b);
+  void clear();
   void printSolution();
   Rest();
   ~Rest();
@@ -52,21 +51,13 @@ state_t operator ++(state_t &s, int) {
   return ( prev_s );
 }
 
-Arc::Arc(int a, int b) {
-  (*this).a=a;
-  this->b=b; //
-}
-
 Rest::Rest() {
-  v = new vector<Arc *>();
+  m = new multimap<int, int>();
 }
 
 Rest::~Rest() {
-  if (v!=NULL) {
-    for (Arc * arc: *v) {
-      delete arc;
-    }
-    delete v;
+  if (m!=NULL) {
+    delete m;
   }
 
   if (s!=NULL) {
@@ -74,18 +65,49 @@ Rest::~Rest() {
   }
 }
 
-Arc * Rest::add(int a, int b) {
-  Arc * arc = new Arc(a,b);
-  v->push_back(arc);
-  return arc;
+void Rest::add(int a, int b) {
+  m->insert(pair<int,int>(a, b));
+  m->insert(pair<int,int>(b, a));
+  size++;
 }
 
+/*
+  Поиском в глубину найти решение.
+  Параметры:
+  i - номер текущей вершины ?
+  p - указатель на предыдущую вершину.
 
-bool Rest::csp(int i, state_t p) {
+ */
+
+typedef multimap<int,int>::iterator int_i_t;
+
+bool Rest::csp(int n, state_t p) {
   state_t c=none;
 
-  for (c=none; c<=BB; c++) {
-    cout << "Value:" << c << NL;
+  for (c=none; c<MAX; c++) {
+    if (n==c) continue; //
+
+    pair<int_i_t,int_i_t> it = m->equal_range(n);
+    s->at(n)=c;  //
+
+    // raise(SIGINT);
+
+    for (int_i_t mit = it.first; mit!=it.second; mit++) {
+      int m = mit->second;
+
+      if (s->at(m) == none) csp(m, c);
+    }
+
+    s->at(n)=none;
+  }
+}
+
+void Rest::clear() {
+  if (s==NULL) {
+    s = new vector<state_t>(size);
+  }
+  for (state_t &i: *s) {
+    i=none;
   }
 }
 
@@ -104,6 +126,8 @@ int main(int argc, char ** argv) {
   // rest.setLen(3);
   rest.add(1,2);
   rest.add(2,3);
+
+  rest.clear();
 
   if (rest.csp(0, none)) {
     cout << "Solution found" << NL;
